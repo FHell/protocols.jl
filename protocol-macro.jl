3be0b1c0da86b44ce84b375b43d1483391135d9d
Frank Hellmann <hellmann@pik-potsdam.de>
2020-05-25 11:52:37 +0200

    V2

--- protocol-macro.jl
+++ protocol-macro.jl
@@ -7,7 +7,7 @@ Protocol Iterable
   # Specify fields the Iterable must have.
   start::Int64
 
-  # Docstrings in Protocol definitions could also serve as documentation for functions
+  # Docstrings in Protocol definitions could serve as documentation for functions
   # rather than methods.
   function next(t :~ Iterable) # no default implementation
 
@@ -21,13 +21,14 @@ end
 
 # We now define a type...
 struct T
-  start::Int64
+  start::Int
   c
 end
 
 # and implement the Protocol for it
 impl Iterable for T
 
+  # Here we also check the function signature is correct  according to the Protocol
   function next(t::T)
     t.c
   end
@@ -45,47 +46,11 @@ end
 # we get an object of type T
 t = T(1, 5)
 
-# and call it, explicitly treating t as Iterable:
-get_third(t :~ Iterable)
-
-# This, on the other hand, should raise an error (Protocol Error: Need to
-# explicitly declare protocol for t!):
+# and call get_third on it:
 get_third(t)
 
 # and calling it this way:
-arr = [1]
-get_third(arr :~ Iterable)
+get_third(1)
 # raises "Type of argument arr does not implement protocol Iterable"
-
-
-# If we violate the protocol we get an immediate error:
-
-struct T2
-  c
-end
-
-impl Iterable for T2
-
-  function next(t::T2)
-    t.c
-  end
-
-end # Protocol Error: T2 has no field "start"
-
-# If we don't have all functions defined we also get an error:
-
-struct T3
-  start::Int64
-  c
-end
-
-impl Iterable for T3
-end # Protocol Error: function next not defined for T3
-
-# This means we can't implement the behaviour after we declare that T3
-# implements the protocol
-
-# I would also favour that the non-default functions need to be explicitly
-# declaredin the impl block.
 
 end #@Prot
